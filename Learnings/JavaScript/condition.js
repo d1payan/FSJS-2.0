f43e12c1fd13f28ec5ea2d6069057799626cc93d
d1payan <csdipayan@gmail.com>
2023-01-07 17:54:32 +0530

    Learned about condition statements in JS

---
+++ Learnings/JavaScript/condition.js
@@ -0,0 +1,10 @@
+// if-else statement
+// if statement evaluates if the condition is true and if the condition is false then else part evaluates
+
+let number = 8;
+
+if (number % 2 === 0) {
+	console.log('Even number');
+} else {
+	console.log('Odd number');
+}
