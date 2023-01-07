b736d73a9d9c33a5828fd1d3b4f1bf87fd866ae9
d1payan <csdipayan@gmail.com>
2023-01-07 22:23:27 +0530

    Learned about switch case and ternary operator

--- Learnings/JavaScript/condition.js
+++ Learnings/JavaScript/condition.js
@@ -8,3 +8,28 @@ if (number % 2 === 0) {
 } else {
 	console.log('Odd number');
 }
+
+// Switch Case
+
+let user = 'Student';
+switch (user) {
+	case 'Admin':
+		console.log('Give access to everything');
+		break;
+	case 'Student':
+		console.log('Give access to the courses he/she purchased');
+		break;
+	case 'Instructor':
+		console.log('Give access to the courses the instructor teaches');
+		break;
+	default:
+		console.log("Explore the site, don't give access to anything");
+}
+
+// Ternary Operator
+
+let m = 20;
+let n = 10;
+m > n
+	? console.log(m, 'is greater than', n)
+	: console.log(n, 'is greater than', m);
