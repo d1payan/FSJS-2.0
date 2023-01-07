32a90fd57696caa905b2f0eabc490addf18acad1
d1payan <csdipayan@gmail.com>
2023-01-07 14:25:12 +0530

    Learned about data types in JavaScript

---
+++ Learnings/JavaScript/data-types.js
@@ -0,0 +1,30 @@
+// Data Types
+// Primitive Data Types => Can have only single value
+
+// Number Values
+5;
+8.5;
+
+// String Values
+('Dipayan');
+('Biswas');
+
+// Boolean Values
+true;
+false;
+
+// Empty Values
+null;
+undefined;
+
+// Non-Primitive Data Types => Can have multiple values
+
+// Array
+[1, 2.8, 7, 'dipayan', true, 'biswas']; // Array index starts from 0
+
+// Object
+{
+	key: value;
+	fname: 'Dipayan';
+	lname: 'Biswas';
+}
