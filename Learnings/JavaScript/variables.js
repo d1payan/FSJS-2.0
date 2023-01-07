b95f9c0135a36c8a2fcb9c8ca2a1da563f9e474a
d1payan <csdipayan@gmail.com>
2023-01-07 14:50:02 +0530

    Learned about variables in JS and print output

---
+++ Learnings/JavaScript/variables.js
@@ -0,0 +1,23 @@
+// We use variables to store some value. Variables work as place holders.
+
+// There are three ways to declare a variable.
+// var, let and const
+// But we use only let and const
+
+var x = 10;
+let y = 'Dipayan'; // Value may change later, means we can assign new value or update the value
+const z = 123456; // Value does not change, means we can't updates its value
+
+let firstName = 'Dipayan';
+let lastName = 'Biswas';
+const mobileNo = 6789034512;
+
+// Print output
+
+console.log('My first name is', firstName);
+console.log('My last name is ' + lastName);
+console.log('My mobile number is', mobileNo);
+
+// Template Literal
+
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
