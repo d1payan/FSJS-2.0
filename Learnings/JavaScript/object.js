220a5dbff2f3e815eb34266606df65c51714a39c
d1payan <csdipayan@gmail.com>
2023-01-18 23:20:21 +0530

    Learned more about object in JS

--- Learnings/JavaScript/object.js
+++ Learnings/JavaScript/object.js
@@ -11,6 +11,7 @@ console.log(bio);
 console.log(bio.firstName);
 
 bio.profession = 'Student'; // Updating the value of profession
+bio['gender'] = 'Male'; // Adding new property to the object
 console.log(bio);
 
 // for in
@@ -19,3 +20,8 @@ for (let x in bio) {
 	// console.log(x); // Gives all the keys
 	console.log(bio[x]); // Gives all the values
 }
+
+// Another way of creating object
+const obj = new Object(); // constructor
+obj.color = 'red';
+console.log(obj);
